f8d4bd1c9eb633af437fc5f289598cb45b3ccd9b
sinuee Hernandez <sinuee@gmail.com>
2017-05-26 16:55:43 -0500

    Completing PlotData

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -18,9 +18,9 @@ figure; % open a new figure window
 
 
 figure 1, plot(x,y, 'rx', 'MarkerSize', 15);
-xlabel('Axis X');
-ylabel('Axis Y');
-title('Data Representation');
+xlabel('Population of City in 10,000s');
+ylabel('Profit in $10,000s');
+title('Population of City vs Profit');
 grid;
 
 % ============================================================
